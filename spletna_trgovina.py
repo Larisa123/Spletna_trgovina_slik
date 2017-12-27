83ff4a614cf7d2ae41c683e2eb9e80489eb3d627
Larisa <lara.carli@gmail.com>
2017-12-27 11:40:44 +0100

    added html

--- spletna_trgovina.py
+++ spletna_trgovina.py
@@ -4,34 +4,24 @@ import modeli # delo z bazo
 from bottle import *
 
 
-class StanjeAplikacije:
-    """ Se obnaša podobno kot enum v drugih jezikih - z pomočjo tega razreda nastavimo 
-    stanje na eno izmed možnih vrednosti, brez da bi si morali zapomniti, katere nize
-    smo uporabili za katero stanje. Po potrebi lahko stanja dodajama. """
-
-    Domov = 1 # gumb Home - to bo privzeta vrednost
-    Opis = 2 # gumb About me
-    Trgovina = 3 # gumb Store
-    Kosarica = 4 # gumb Basket - do tega lahko dostopamo samo v trgovini
-    Racun = 5 # ko zaključimo nakup, se nam prikaže nova stran, na kateri je račun nakupa
-
-
-stanjeAp = StanjeAplikacije.Domov
-
 # Metode za prikazovanje ustreznih strani:
 
 @get('/')
 def prikaziMenuDomov():
-    """ Morema napisati html """
-    return template('domov.html')
+    return template('domov2.html')
 
+@get('/static/<filename:path>')
+def static(filename):
+    return static_file(filename, root='static')
+
+@get('/aboutme')
 def prikaziMenuOpis():
-    """ Morema napisati html """
-    pass
+    return template('aboutme.html')
 
+@get('/store')
 def prikaziMenuTrgovina():
-    """ Morema napisati html """
-    pass
+    return template('store.html')
+
 
 def prikaziKosarico():
     """ Uporabnika napoti na novo stran, na kateri mu prikaže košarico v obliki seznama, tako da
@@ -43,20 +33,6 @@ def prikaziKosarico():
 def prikaziRacun():
     pass
 
-
-def nalozi():
-    """ Nalozi ustrezno stran. """
-    if stanjeAp == StanjeAplikacije.Domov:
-        prikaziMenuDomov()
-    elif stanjeAp == StanjeAplikacije.Opis:
-        prikaziMenuOpis()
-    elif stanjeAp == StanjeAplikacije.Trgovina:
-        prikaziMenuTrgovina()
-    elif stanjeAp == StanjeAplikacije.Kosarica:
-        prikaziKosarico()
-    elif stanjeAp == StanjeAplikacije.Racun:
-        prikaziRacun()
-
 # Pomožne metode:
 
 def izpisiVsePodatkeTabele(tabela):
@@ -67,4 +43,3 @@ def izpisiVsePodatkeTabele(tabela):
 
 run(host='localhost', port=8080, reloader=True)
 
-nalozi()
